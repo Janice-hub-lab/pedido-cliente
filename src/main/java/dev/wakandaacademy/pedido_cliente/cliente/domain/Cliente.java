9324b8b8a0566fd5b2b72a7276151cc1223f7d5d
Jane Gomes <asscontecjane@gmail.com>
2024-10-14 16:16:03 -0700

    ajusta mapeamento id uuid

--- src/main/java/dev/wakandaacademy/pedido_cliente/cliente/domain/Cliente.java
+++ src/main/java/dev/wakandaacademy/pedido_cliente/cliente/domain/Cliente.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -18,6 +21,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Cliente {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private UUID idCliente;
 	@NotBlank
     private String nomeCompleto;
@@ -34,8 +39,6 @@ public class Cliente {
 	
 	private Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email, @NotBlank String celular,
 			@CPF String cpf) {
-		super();
-		this.idCliente = UUID.randomUUID();
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
 		this.celular = celular;
