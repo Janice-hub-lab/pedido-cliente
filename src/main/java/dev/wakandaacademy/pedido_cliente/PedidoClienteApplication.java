ced43c325926205f5140c39dd7fd2e3a8a51c708
Jane Gomes <asscontecjane@gmail.com>
2024-10-14 14:06:13 -0700

    implementa api de teste

--- src/main/java/dev/wakandaacademy/pedido_cliente/PedidoClienteApplication.java
+++ src/main/java/dev/wakandaacademy/pedido_cliente/PedidoClienteApplication.java
@@ -2,10 +2,18 @@ package dev.wakandaacademy.pedido_cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PedidoClienteApplication {
-
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cliente-Pedido - API Home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoClienteApplication.class, args);
 	}
