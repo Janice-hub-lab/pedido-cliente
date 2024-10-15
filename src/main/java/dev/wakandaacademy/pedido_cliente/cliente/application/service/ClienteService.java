cc3c0aa2bc65755fcbba35d7109ed10880f3b50a
Jane Gomes <asscontecjane@gmail.com>
2024-10-14 17:51:00 -0700

    implementa controller e fluxo para service

---
+++ src/main/java/dev/wakandaacademy/pedido_cliente/cliente/application/service/ClienteService.java
@@ -0,0 +1,10 @@
+package dev.wakandaacademy.pedido_cliente.cliente.application.service;
+
+import dev.wakandaacademy.pedido_cliente.cliente.application.api.ClienteRequest;
+import dev.wakandaacademy.pedido_cliente.cliente.application.api.ClienteResponse;
+
+public interface ClienteService {
+
+	ClienteResponse criaCliente(ClienteRequest clienteRequest);
+
+}
