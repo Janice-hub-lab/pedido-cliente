5df81c0c9da59145341ce59255f3b5b5eb3f3242
Jane Gomes <asscontecjane@gmail.com>
2024-10-15 11:19:38 -0700

    implementa fluxo do repository. Teste ok! e configura swagger

---
+++ src/main/java/dev/wakandaacademy/pedido_cliente/cliente/infra/ClienteInfraRepository.java
@@ -0,0 +1,20 @@
+package dev.wakandaacademy.pedido_cliente.cliente.infra;
+
+import org.springframework.stereotype.Repository;
+
+import dev.wakandaacademy.pedido_cliente.cliente.application.repository.ClienteRepository;
+import dev.wakandaacademy.pedido_cliente.cliente.domain.Cliente;
+import lombok.extern.log4j.Log4j2;
+
+@Repository
+@Log4j2
+public class ClienteInfraRepository implements ClienteRepository {
+
+	@Override
+	public Cliente salva(Cliente cliente) {
+		log.info("[inicia] ClienteInfraRepository - salva");
+		log.info("[finaliza] ClienteInfraRepository - salva");
+		return cliente;
+	}
+
+}
